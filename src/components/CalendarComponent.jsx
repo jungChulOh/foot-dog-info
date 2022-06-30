0adafb0730211eb88af30632fa78c1840a5fc5a0
Develop-Jung <jungjob1114@gmail.com>
2022-07-01 00:03:53 +0900

    달력 수정중...2

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -5,14 +5,22 @@ import googleCalendarPlugin from "@fullcalendar/google-calendar";
 
 export default function CalenderComponent() {
   const API_KEY = process.env.REACT_APP_CALENDER_ID;
+  const initialView = window.outerWidth < 640 ? "dayGridDay" : "dayGridWeek";
   return (
-    <Fullcalender
-      plugins={[dayGridPlugin, googleCalendarPlugin]}
-      googleCalendarApiKey={API_KEY}
-      events={{
-        googleCalendarId: "fcfootdog@gmail.com",
-      }}
-      initialView="dayGridMonth"
-    />
+    <div className="p-3 w-full h-full overflow-x-auto">
+      <Fullcalender
+        viewClassNames={["sans", "text-xs"]}
+        plugins={[dayGridPlugin, googleCalendarPlugin]}
+        googleCalendarApiKey={API_KEY}
+        events={{
+          googleCalendarId: "fcfootdog@gmail.com",
+          display: "block",
+          classNames: ["text-xs"],
+        }}
+        initialView={initialView}
+        locale="ko"
+        headerToolbar={{ left: "", center: "title", right: "" }}
+      />
+    </div>
   );
 }
