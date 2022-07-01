235e490d51be07b27839dd697ceb1e823ef976db
choj <jungjob1114@gmail.com>
2022-07-01 10:31:05 +0900

    모바일, PC에 따른 달력 동적 표시

--- src/components/CalendarComponent.jsx
+++ src/components/CalendarComponent.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Fullcalender from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import listPlugin from "@fullcalendar/list";
 import googleCalendarPlugin from "@fullcalendar/google-calendar";
+import styled from "styled-components";
+
+const CalendarDiv = styled.div`
+  .fc-toolbar-title {
+    font-size: 1.25rem;
+    line-height: 1.75rem;
+  }
+`;
 
 export default function CalenderComponent() {
   const API_KEY = process.env.REACT_APP_CALENDER_ID;
-  const initialView = window.outerWidth < 640 ? "dayGridDay" : "dayGridWeek";
+  const initialView = window.outerWidth < 640 ? "listMonth" : "dayGridMonth";
+  const contentHeight = window.outerWidth < 640 ? "300px" : "500px";
   return (
-    <div className="p-3 w-full h-full overflow-x-auto">
+    <CalendarDiv className="p-3 w-full h-full overflow-x-auto">
       <Fullcalender
         viewClassNames={["sans", "text-xs"]}
-        plugins={[dayGridPlugin, googleCalendarPlugin]}
+        plugins={[dayGridPlugin, listPlugin, googleCalendarPlugin]}
         googleCalendarApiKey={API_KEY}
         events={{
           googleCalendarId: "fcfootdog@gmail.com",
@@ -20,7 +30,11 @@ export default function CalenderComponent() {
         initialView={initialView}
         locale="ko"
         headerToolbar={{ left: "", center: "title", right: "" }}
+        contentHeight={contentHeight}
+        eventClick={(info) => {
+          info.jsEvent.preventDefault();
+        }}
       />
-    </div>
+    </CalendarDiv>
   );
 }
