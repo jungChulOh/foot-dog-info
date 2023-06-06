484a72061aae28799b8226a04481ae0b229454aa
O.IT <jungjob1114@gmail.com>
2023-06-06 22:45:16 +0900

    1. 가입신청서 버튼 navbar, footer content 에 추가 2. 신청서, 가입 문의 에 대한 버튼에 아이콘 제거 3. 버튼 색상 변경

--- src/components/footer/Content.jsx
+++ src/components/footer/Content.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import kakaoSpeechBubbleImg from "@/images/icons/kakaotalk_btn/kakao-speech-bubble.png";
 
 export default function Content() {
   return (
@@ -11,23 +10,37 @@ export default function Content() {
         <p className="text-xl md:text-3xl font-bold mb-6">
           우리는 FC풋도그 입니다!
         </p>
-        <a
-          href="https://open.kakao.com/me/FC_Footdog"
-          target="_blank"
-          rel="noreferrer"
-          className="flex flex-row items-center transition ease-in-out bg-yellow-300 rounded-3xl text-white mb-6 shadow-lg focus:ring-2 focus:ring-inset focus:ring-indigo-500 focus:scale-110 focus:-translate-y-2"
-        >
-          <div className="p-4 rounded-l-3xl flex flex-row justify-between">
-            <img src={kakaoSpeechBubbleImg} className="w-6 h-auto" />
-          </div>
-          <p className="p-4 pl-0">가입 문의</p>
-        </a>
+        <div className="flex flex-row justify-between items-center">
+          <a
+            href="https://forms.gle/mu65P214oCS8yLvF8"
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-row items-center transition ease-in-out bg-sky-700 rounded-3xl text-white mb-6 shadow-lg focus:ring-2 focus:ring-inset focus:ring-indigo-500 focus:scale-110 focus:-translate-y-2 mr-5"
+          >
+            <p className="p-4 font-bold"> 가입신청서 작성</p>
+          </a>
+          <a
+            href="https://open.kakao.com/me/FC_Footdog"
+            target="_blank"
+            rel="noreferrer"
+            className="flex flex-row items-center transition ease-in-out bg-yellow-400 rounded-3xl text-amber-950 mb-6 shadow-lg focus:ring-2 focus:ring-inset focus:ring-indigo-500 focus:scale-110 focus:-translate-y-2"
+          >
+            <p className="p-4 font-bold">가입문의</p>
+          </a>
+        </div>
         <p className="font-light text-gray-400 mb-10">
           가입은 항상 열려 있습니다 👋
         </p>
         <p className="text-[0.5rem] md:text-xs font-light text-gray-400">
-          &copy; {new Date().getFullYear()} FC FootDog Team ⚽ Created By
-          JungChulOh
+          &copy; {new Date().getFullYear()} FC FootDog Team ⚽ Created By{" "}
+          <a
+            className="font-bold text-sky-700"
+            target="_blank"
+            href="https://github.com/jungChulOh"
+            rel="noopener noreferrer"
+          >
+            Iron.J
+          </a>
         </p>
       </div>
     </div>
